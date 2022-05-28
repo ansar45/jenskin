ac3da9d9d8eca80de3bdf6d921060056636cbe7a
root <root@localhost.localdomain>
2022-05-28 02:53:37 +0000

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,13 @@
+
+de{
+    stage('Clone') {
+        checkout scm
+    }
+    stage('Ansible') {
+      ansiblePlaybook (
+          colorized: true,          
+          playbook: 'playbook.yml',
+          inventory: 'hosts.yml'
+      )
+    }
+}
---
+++ remove_db.php
@@ -0,0 +1,25 @@
+<?php
+
+   $dbhost = '{{ host }}';
+   $dbuser = '{{ user }}';
+   $dbpass = '{{ password }}';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   echo "Connected successfully\n";
+   
+   $sql = 'DROP Database test_db';
+   $retval = mysql_query( $sql, $conn );
+   
+   echo  "Database test removed successfully";
+
+   if(! $retval ) {
+      die('Could not create database: ' . mysql_error());
+   
+
+   }
+   mysql_close($conn);
+?>
